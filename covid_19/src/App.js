72e06f5ae91bbd604993c6fac4f223a976e3ac98
ngabomugisha <ngabo.mugisha.bro@gmail.com>
2020-08-07 19:39:25 +0200

    added flex

--- covid_19/src/App.js
+++ covid_19/src/App.js
@@ -1,70 +1,88 @@
-import React ,{ useState, useEffect} from 'react';
-import './App.css';
-import {MenuItem, FormControl, Select,} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 
 function App() {
-
-  const[countries, setCountries] = useState([]);
-  const [country, setCountry] =useState('worldwide');
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   useEffect(() => {
-    // the code inside here will run once 
+    // the code inside here will run once
     // when the component loads and not again after
 
-    const getCountriesData =async () => {
-      await fetch ("https://disease.sh/v3/covid-19/countries")
-      .then((response) => response.json())
-      .then((data) =>{
-        const countries = data.map((country) => ({
-          name: country.country, // rwanda, burundu
-          value: country.countryInfo.iso2 // rw, us, ug
-        }));
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // rwanda, burundu
+            value: country.countryInfo.iso2, // rw, us, ug
+          }));
 
-        setCountries(countries);
-
-      });
+          setCountries(countries);
+        });
     };
 
     getCountriesData();
-
-  },[]);
+  }, []);
 
   const onCountryChange = async (e) => {
     const countryCode = e.target.value;
-    console.log("hhhhhhhhhhhhhhhhhhhhhh>>>>>>>>>>>>>>>>....", countryCode)
-    setCountry(countryCode)
-  }
+    console.log("hhhhhhhhhhhhhhhhhhhhhh>>>>>>>>>>>>>>>>....", countryCode);
+    setCountry(countryCode);
+  };
 
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldWide">WorldWide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        {/* header */}
+        {/* Title + select input ropdown field */}
 
-    <div className="app__header">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value={country} onChange={onCountryChange}>
-            <MenuItem value="worldWide">WorldWide</MenuItem>
-          {countries.map((country) =>(
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
-      {/* header */}
-      {/* Title + select input ropdown field */}
+        <div className="app__stats">
+          {/* InfoBoxes */}
 
+          <InfoBox title="CoronaVirus cases" total={2000} cases={20} />
 
-<div className="app__stats">
-  
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
+          {/* InfoBoxes */}
 
+          <InfoBox title="Recovered" total={3000} cases={20} />
+
+          {/* InfoBoxes */}
+
+          <InfoBox title="Deaths" total={100} cases={20} />
+        </div>
+
+        {/* Table */}
+        {/* Graph */}
+
+        {/* Map */}
+        <Map />
       </div>
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Case by Country</h3>
+        </CardContent>
+        {/* Table */}
+        <h3>worldwide new case</h3>
+        {/* graph */}
+      </Card>
     </div>
   );
 }
---
+++ covid_19/src/InfoBox.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Card, CardContent, Typography} from "@material-ui/core"
+
+function InfoBox({title, cases, total}) {
+    return (
+        <Card className="infoBox">
+            <CardContent>
+                {/* title */}
+
+                <Typography className="infoBox__title" color="textSecondary">
+
+                    {title}
+
+                </Typography>
+
+
+                {/* number of cases */}
+
+                    <h2 className="infoBox__cases">{cases}</h2>
+
+                {/* Total */}
+
+                    <Typography className="infoBox__total" color="textSecondary">
+
+                        {total} Total
+
+                    </Typography>
+
+
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
