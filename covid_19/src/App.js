382eaafeecc913241a510a2408027b9937c396b3
ngabomugisha <ngabo.mugisha.bro@gmail.com>
2020-08-07 18:30:06 +0200

    started to build covid-19 tracker

--- covid_19/src/App.js
+++ covid_19/src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{ useState, useEffect} from 'react';
 import './App.css';
+import {MenuItem, FormControl, Select,} from "@material-ui/core";
 
 function App() {
+
+  const[countries, setCountries] = useState([])
+
+  useEffect(() => {
+    // the code inside here will run once 
+    // when the component loads and not again after
+
+    const getCountriesData =async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) =>{
+        const countries = data.map((country) => ({
+          name: country.country, // rwanda, burundu
+          value: country.countryInfo.iso2 // rw, us, ug
+        }));
+
+        setCountries(countries);
+
+      });
+    };
+
+    getCountriesData();
+
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app__header">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          {countries.map((country) =>(
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+      {/* header */}
+      {/* Title + select input ropdown field */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
